2fadf5b84f4d35f0fdf824b21fdcabfb75994235
Tytan-Codes <tytan_official@proton.me>
2024-11-02 12:01:33 -0400

    created time app everything working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,25 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import { Cormorant_Garamond, Poppins, Koh_Santepheap } from "next/font/google";
+
+const koh = Koh_Santepheap({
+  subsets: ["latin"],
+  variable: "--font-koh",
+  weight: ["400", "700"],
+})
+const cormorantGaramond = Cormorant_Garamond({
+  subsets: ["latin"],
+  variable: "--font-cormorant-garamond",
+  weight: ["400"],
+})
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "600"],
+})
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +45,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${cormorantGaramond.variable} ${poppins.variable} ${koh.variable} antialiased`}
       >
-        {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
